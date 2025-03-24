b44bd189f69ab316e2540fb1541131697b6a5d8d
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2025-03-24 20:53:16 -0300

    chore: add /dist to gitignore

--- dist/background.js
+++
@@ -1,7 +0,0 @@
-// Open options page when user clicks the extension icon
-const action = typeof browser !== 'undefined' ? browser.browserAction : chrome.browserAction;
-const runtime = typeof browser !== 'undefined' ? browser.runtime : chrome.runtime;
-
-action.onClicked.addListener((tab) => {
-  runtime.openOptionsPage();
-});
