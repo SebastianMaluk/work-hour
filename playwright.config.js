7c24a02c40c793468745b3bd79899430319a2ff2
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2025-03-24 21:03:24 -0300

    feat: use github report on playwright ci

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
   retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
